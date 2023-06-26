bd63d236bc6f19d842ca7f27ece5e7cc3e30e11a
mahdi-shi <4932abcd@gmail.com>
2023-06-26 12:39:38 +0330

    css and bootstrap files added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
 app.listen(3000 , () => console.log("listening at port 3000"));
-app.use(express.static('public'));
+app.use(express.static('public'));  
