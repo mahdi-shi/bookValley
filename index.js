c26f4d7f2f26fc01b1779ec409e8a6790b3a75d3
Tailus Dev <4932abcd@gmail.com>
2023-07-30 17:24:00 +0430

    nedb database for saving user's information installed

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const DataStore = require('nedb');
+
 const app = express();
 app.listen(3000 , () => console.log("listening at port 3000"));
 app.use(express.static('public'));  
+app.use(express.json({limit : '1mb'}))
+
+const dataBase = new DataStore('database.db')
+dataBase.loadDatabase();
