991ccf1f9d980597cbabd01fe85eb64f8dffd3ac
Tailus Dev <4932abcd@gmail.com>
2023-08-02 10:56:58 +0430

    searching into database for know that user is already signed up or not fixed

--- index.js
+++ index.js
@@ -2,22 +2,41 @@ const express = require('express');
 const DataStore = require('nedb');
 
 const app = express();
-app.listen(3000 , () => console.log("listening at port 3000"));
-app.use(express.static('public'));  
-app.use(express.json({limit : '1mb'}))
+app.listen(3000, () => console.log("listening at port 3000"));
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }))
 
 const dataBase = new DataStore('database.db')
 dataBase.loadDatabase();
 
-app.post("/UsersData",(req,res) => {
+app.post("/UsersData", (req, res) => {
     console.log("i got the reques")
-    console.log(req.body)
-    dataBase.insert({userName : req.body.userName,email : req.body.emailAddress,password : req.body.Password})
-    dataBase.find({},(err,data) => {
-        if(err){
+    dataBase.find({}, (error, Data) => {
+        if (error) {
             res.end();
             return;
         }
-        req.json(data)
+        let userNameExsistingStatus = true;
+
+        for (let i = 0; i < Data.length; i++) {
+            if (Data[i].userName == req.body.userName || Data[i].email == req.body.emailAddress) {
+                userNameExsistingStatus = false;
+                break;
+            }
+        }
+
+        if (userNameExsistingStatus == false) {
+            res.json({
+                status: " it's there"
+            })
+            console.log("it's there");
+        }
+        else {
+            dataBase.insert({ userName: req.body.userName, email: req.body.emailAddress, password: req.body.Password })
+            res.json({
+                status: "it's not there"
+            })
+            console.log("it's not there");
+        }
     })
 })
