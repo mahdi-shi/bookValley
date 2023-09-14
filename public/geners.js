b4713ccf8c84ec76325b4c80a29814ecf4b1ed61
Tailus Dev <4932abcd@gmail.com>
2023-09-14 15:42:13 +0430

    some desing in navbar profile and gener paragraph changed

--- public/geners.js
+++ public/geners.js
@@ -64,73 +64,92 @@ for (let i = 0; i < subjects.length; i++) {
                 bookCovers[i].src = `https://covers.openlibrary.org/b/id/${data.works[i].cover_id}-M.jpg`
             }
         }
+
+        searchPnl.style.marginTop = "-200vh";
+        searchPnl.style.opacity = "0";
+        bookSearchTxtBox.value = "";
+        const books = document.querySelectorAll(".li");
+        if (books.length > 0) {
+            for (let i = 0; i < books.length; i++) {
+                books[i].remove();
+            }
+        }
     })
 }
 
-    //searching stuff
+//searching stuff
 
-    const searchPnl = document.querySelector(".searchPnl");
-    const searchPnlCloseBtn = document.querySelector("#searchPnlCloseBtn");
-    const bookSearchTxtBox = document.querySelector("#bookSearchTxtBox");
-    const secondetList = document.querySelector(".secondetList");
-    const messageBox = document.querySelector("#messageBox");
+const searchPnl = document.querySelector(".searchPnl");
+const searchPnlCloseBtn = document.querySelector("#searchPnlCloseBtn");
+const bookSearchTxtBox = document.querySelector("#bookSearchTxtBox");
+const secondetList = document.querySelector(".secondetList");
+const messageBox = document.querySelector("#messageBox");
 
-    searchBoxBtn.addEventListener("click", async () => {
+searchBoxBtn.addEventListener("click", async () => {
 
-        if (bookSearchTxtBox.value == "") {
-            messageBox.style.top = "7%"
-            setTimeout(() => {
-                messageBox.style.top = "-10%"
-            }, 4000);
-        }
-        else {
-            searchPnl.style.marginTop = "-9vh";
-            searchPnl.style.opacity = "0.95";
+    if (bookSearchTxtBox.value == "") {
+        messageBox.style.top = "7%"
+        setTimeout(() => {
+            messageBox.style.top = "-10%"
+        }, 4000);
+    }
+    else {
+        searchPnl.style.marginTop = "-9vh";
+        searchPnl.style.opacity = "0.95";
 
-            const response = await fetch("https://openlibrary.org/search.json?q=" + bookSearchTxtBox.value)
-            const data = await response.json();
+        const response = await fetch("https://openlibrary.org/search.json?q=" + bookSearchTxtBox.value)
+        const data = await response.json();
 
-            const books = document.querySelectorAll(".li");
+        const books = document.querySelectorAll(".li");
 
-            if (books.length > 0) {
-                for (let i = 0; i < books.length; i++) {
-                    books[i].remove();
-                }
-            }
-
-            for (let i = 0; i < data.docs.length; i++) {
-
-                console.log(data.docs[i].title);
-
-                let li = document.createElement("li");
-                let div = document.createElement("div");
-                let p = document.createElement("p");
-                let img = document.createElement("img");
-
-                let titlePara = document.createTextNode(data.docs[i].title);
-                p.appendChild(titlePara);
-                p.classList.add("bookTitle");
-                img.classList.add("bookCover");
-                if (data.docs[i].cover_i == null) {
-                    img.src = "assets/images (1).jfif";
-                }
-                else {
-                    img.src = "https://covers.openlibrary.org/b/id/" + data.docs[i].cover_i + "-M.jpg";
-                }
-
-                div.classList.add("books");
-                div.appendChild(p);
-                div.appendChild(img);
-                div.style = "--i:" + i;
-                li.classList.add("li");
-                li.appendChild(div);
-                secondetList.appendChild(li)
-
+        if (books.length > 0) {
+            for (let i = 0; i < books.length; i++) {
+                books[i].remove();
             }
         }
-    })
 
-    bookSearchTxtBox.addEventListener("keydown", async (e) => {
+        for (let i = 0; i < data.docs.length; i++) {
+
+            console.log(data.docs[i].title);
+
+            let li = document.createElement("li");
+            let div = document.createElement("div");
+            let p = document.createElement("p");
+            let img = document.createElement("img");
+
+            let titlePara = document.createTextNode(data.docs[i].title);
+            p.appendChild(titlePara);
+            p.classList.add("bookTitle");
+            img.classList.add("bookCover");
+            if (data.docs[i].cover_i == null) {
+                img.src = "assets/images (1).jfif";
+            }
+            else {
+                img.src = "https://covers.openlibrary.org/b/id/" + data.docs[i].cover_i + "-M.jpg";
+            }
+
+            div.classList.add("books");
+            div.appendChild(p);
+            div.appendChild(img);
+            div.style = "--i:" + i;
+            li.classList.add("li");
+            li.appendChild(div);
+            secondetList.appendChild(li)
+
+        }
+    }
+    generBooksPnl.style.left = "-100%";
+    generBooksPnl.style.opacity = "0";
+    backgroundCover.style.opacity = 0;
+    navBar.style.backgroundColor = "transparent";
+    for (let i = 0; i < bookTitles.length; i++) {
+        bookTitles[i].innerHTML = "";
+        bookCovers[i].style.opacity = 0;
+        bookCovers[i].src = "";
+    }
+})
+
+bookSearchTxtBox.addEventListener("keydown", async (e) => {
     const keyName = e.key;
     if (keyName == "Enter") {
         if (bookSearchTxtBox.value == "") {
@@ -183,29 +202,65 @@ for (let i = 0; i < subjects.length; i++) {
                 secondetList.appendChild(li)
 
             }
+            generBooksPnl.style.left = "-100%";
+            generBooksPnl.style.opacity = "0";
+            backgroundCover.style.opacity = 0;
+            navBar.style.backgroundColor = "transparent";
+            for (let i = 0; i < bookTitles.length; i++) {
+                bookTitles[i].innerHTML = "";
+                bookCovers[i].style.opacity = 0;
+                bookCovers[i].src = "";
+            }
         }
     }
 })
 
-    searchPnlCloseBtn.addEventListener("click", () => {
-        searchPnl.style.marginTop = "-200vh";
-        searchPnl.style.opacity = "0";
-        bookSearchTxtBox.value = "";
-        const books = document.querySelectorAll(".li");
-        if (books.length > 0) {
-            for (let i = 0; i < books.length; i++) {
-                books[i].remove();
-            }
-        }
-
-    })
-
-    searchPnl.onscroll = () => {
-
-        if (searchPnl.scrollTop > 100) {
-            navBar.style.backgroundColor = "#242131";
-        }
-        if (searchPnl.scrollTop < 100) {
-            navBar.style.backgroundColor = "transparent";
+searchPnlCloseBtn.addEventListener("click", () => {
+    searchPnl.style.marginTop = "-200vh";
+    searchPnl.style.opacity = "0";
+    bookSearchTxtBox.value = "";
+    const books = document.querySelectorAll(".li");
+    if (books.length > 0) {
+        for (let i = 0; i < books.length; i++) {
+            books[i].remove();
         }
     }
+
+})
+
+searchPnl.onscroll = () => {
+
+    if (searchPnl.scrollTop > 100) {
+        navBar.style.backgroundColor = "#242131";
+    }
+    if (searchPnl.scrollTop < 100) {
+        navBar.style.backgroundColor = "transparent";
+    }
+}
+
+document.body.onload = async () => {
+    const userName = localStorage.getItem("userTarget");
+    const data = { userName }
+    const options = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+
+    const response = await fetch("/dataForProfile", options)
+    const dataResponse = await response.json();
+
+    if (dataResponse.image == null) {
+        profIconPicture.style.display = "none";
+        profilePictureSvg.style.display = 'block';
+        profileIconSvg.style.display = "block";
+    }
+    else {
+        profIconPicture.style.display = "block";
+        profIconPicture.src = dataResponse.image;
+        profilePictureSvg.style.display = 'none';
+        profileIconSvg.style.display = "none";
+    }
+}
